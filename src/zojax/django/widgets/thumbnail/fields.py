60199ceabed09fee345cadfd13dcb087a1efeeb6
bubenkoff <bubenkoff@65ff189f-35ee-0310-953c-23049635c3cf>
2010-03-25 15:39:50 +0000

    product company changes

--- src/zojax/django/widgets/thumbnail/fields.py
+++ src/zojax/django/widgets/thumbnail/fields.py
@@ -1,8 +1,12 @@
 from django import forms
 
 from form_utils.fields import ClearableImageField
-from widgets import ImageWidget
+from widgets import AdminImageWidget, ImageWidget
 
 
 class ImageWithThumbnailsField(ClearableImageField):
     widget = ImageWidget
+
+
+class ImageWithThumbnailsAdminField(ClearableImageField):
+    widget = AdminImageWidget
